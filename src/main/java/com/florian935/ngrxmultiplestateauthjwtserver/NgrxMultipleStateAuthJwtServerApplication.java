9586cb1c427878ff5acd62cbd6f5979950b25357
Florian <florian.martin63000@gmail.com>
2021-05-10 20:59:07 +0200

    Feat: initial commit

---
+++ src/main/java/com/florian935/ngrxmultiplestateauthjwtserver/NgrxMultipleStateAuthJwtServerApplication.java
@@ -0,0 +1,13 @@
+package com.florian935.ngrxmultiplestateauthjwtserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NgrxMultipleStateAuthJwtServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NgrxMultipleStateAuthJwtServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/florian935/ngrxmultiplestateauthjwtserver/NgrxMultipleStateAuthJwtServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.florian935.ngrxmultiplestateauthjwtserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NgrxMultipleStateAuthJwtServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
