6e8f6872bf5a3fa4331518444a78753a72950a1c
Florian <florian.martin63000@gmail.com>
2021-05-10 23:16:33 +0200

    Feat: add CORS configuration

--- src/main/java/com/florian935/ngrxmultiplestateauthjwtserver/config/WebConfig.java
+++ src/main/java/com/florian935/ngrxmultiplestateauthjwtserver/config/WebConfig.java
@@ -1,21 +1,14 @@
 package com.florian935.ngrxmultiplestateauthjwtserver.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Collections;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
-    @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("http://localhost:4200")
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
---
+++ src/main/java/com/florian935/ngrxmultiplestateauthjwtserver/config/WebSecurityConfig.java
@@ -0,0 +1,22 @@
+package com.florian935.ngrxmultiplestateauthjwtserver.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                    .csrf().disable()
+                    .cors()
+                .and()
+                    .authorizeRequests()
+                    .anyRequest().permitAll();
+    }
+}
