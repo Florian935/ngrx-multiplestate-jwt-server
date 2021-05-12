6b2b6f4082778494b831efadeb6b035804432886
Florian <florian.martin63000@gmail.com>
2021-05-12 15:13:29 +0200

    Feat: setting CORS

--- src/main/java/com/florian935/ngrxmultiplestateauthjwtserver/config/WebSecurityConfig.java
+++ src/main/java/com/florian935/ngrxmultiplestateauthjwtserver/config/WebSecurityConfig.java
@@ -1,14 +1,12 @@
 package com.florian935.ngrxmultiplestateauthjwtserver.config;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
 @RequiredArgsConstructor
-@Order(2)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
--- src/main/java/com/florian935/ngrxmultiplestateauthjwtserver/controller/PostController.java
+++ src/main/java/com/florian935/ngrxmultiplestateauthjwtserver/controller/PostController.java
@@ -15,6 +15,7 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 @RestController
 @RequestMapping("/api/v1.0/posts")
 @RequiredArgsConstructor
+@CrossOrigin("http://localhost:4200")
 public class PostController {
 
     private final PostService postService;
